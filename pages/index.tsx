b6291f4dec2c44982f8455e8b385dd58f99647fa
andrevenancio <info@andrevenancio.com>
2022-12-01 15:13:27 +0000

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://ultra.unity.com">Ultra</a>
         </h1>
-        <p>NEXTAUTH_URL: {process.env.NEXTAUTH_URL}</p>
         <p className={styles.description}>
           <Button />
         </p>
