66113c073317f4b61b160411e7e05a225ca7ce05
andrevenancio <info@andrevenancio.com>
2022-12-01 14:55:44 +0000

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,5 +9,19 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async redirect(params: { url: string }) {
+      const { url } = params
+
+      // url is just a path, e.g.: /videos/pets
+      if (!url.startsWith("http")) return url
+
+      // If we have a callback use only its relative path
+      const callbackUrl = new URL(url).searchParams.get("callbackUrl")
+      if (!callbackUrl) return url
+
+      return new URL(callbackUrl as string).pathname
+    },
+  },
 }
 export default NextAuth(authOptions)
