47f2b08c234adc926d026cf60936b71e7d722b34
andrevenancio <info@andrevenancio.com>
2022-12-01 15:17:31 +0000

    new test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+  swcMinify: false,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 }
 
 module.exports = nextConfig
